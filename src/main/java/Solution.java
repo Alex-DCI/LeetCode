b817eb43422e7dfe69dedf1f6588162a2a95f28b
Alex <os.owcharenko@gmail.com>
2026-03-10 16:56:26 +0100

    1. Two Sum

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,21 +1,26 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     static void main() {
-        System.out.println(new Solution().isAnagram("car", "rat"));
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
 
-    public boolean isAnagram(String s, String t) {
-        int length = s.length();
-        if (length != t.length()) return false;
-        short[] counter = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        for (int i = 0; i < length; i++) {
-            counter[s.charAt(i) - 97]++;
-            counter[t.charAt(i) - 97]--;
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> resultMap = new HashMap<>();
+        resultMap.put(nums[0], 0);
+
+        for (int i = 1; i < nums.length; i++){
+            int complement = target - nums[i];
+
+            if(resultMap.containsKey(complement)) {
+                return new int[]{resultMap.get(complement), i};
+            }
+
+            resultMap.put(nums[i],i);
         }
-        for (short i : counter) {
-            if (i != 0) return false;
-        }
-        return true;
+
+        return new int[]{};
     }
 }
